48ed556d604fe400e730b4d2f80fa078f4771cc1
fredson <fredsouza.dev@gmail.com>
2023-02-07 08:00:57 -0400

    route of authentication created

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const { Router } = require("express");
 const usersRouter = require("./users.routes");
 const dishesRouter = require("./dishes.routes");
 const ingredientsRouter = require("./ingredients.routes");
+const sessionsRouter = require("./sessions.routes");
 
 const routes = Router();
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
 routes.use("/dishes", dishesRouter);
 routes.use("/ingredients", ingredientsRouter);
 
